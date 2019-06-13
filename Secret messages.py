60aa6608e0a31e3561ff06c41d229d1ca4d50455
souravarya07 <42316833+souravarya07@users.noreply.github.com>
2019-06-13 23:07:33 +0530

    Add files via upload

---
+++ Secret messages.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 2
+
+newMessage = ''
+message = input('Enter the message: ')
+for character in message: 
+    if character in alphabet:
+        position = alphabet.find(character)
+        newPosition = (position + key)%26
+        newCharacter = alphabet[newPosition]
+        #print('the new character is :',newCharacter)
+        newMessage += newCharacter
+  
+    
+print("The secret message for the input message is: ",newMessage)
+    
+
